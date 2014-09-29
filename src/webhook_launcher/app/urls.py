1317d7c518278c43cd96e0719fa404c5764a4cd5
David Greaves <david@dgreaves.com>
2014-09-29 18:50:40 +0100

    Don't use the deprecated 'from' form
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- src/webhook_launcher/app/urls.py
+++ src/webhook_launcher/app/urls.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.contrib import admin
 admin.autodiscover()
 from rest_framework import routers
