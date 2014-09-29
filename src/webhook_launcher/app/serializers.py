886cd384ce3bb9beef3f3fee226b991a0a0218df
David Greaves <david@dgreaves.com>
2014-09-29 22:24:38 +0100

    Add fields to handle serialising the WebHookMapping obs can only be referred to by namespace and must exist user is accessed by username and must exist lsr is returned as nested json and cannot be modified/created in PUT
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- src/webhook_launcher/app/serializers.py
+++ src/webhook_launcher/app/serializers.py
@@ -1,4 +1,5 @@
 from models import WebHookMapping, LastSeenRevision, BuildService
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class BuildServiceSerializer(serializers.ModelSerializer):
@@ -11,10 +12,44 @@ class LastSeenRevisionSerializer(serializers.ModelSerializer):
         model = LastSeenRevision
         fields = ('revision', 'tag', 'timestamp')
 
-class WebHookMappingSerializer(serializers.ModelSerializer):
-    lsr = LastSeenRevisionSerializer()
+class BuildServiceField(serializers.WritableField):
+    """
+    Handle references to a BuildService object
+    Outputs namespace
+    Takes a namespace as a key
+    """
+    def to_native(self, obj):
+        return obj.namespace
 
+    def from_native(self, data):
+        obs = BuildService.objects.get(namespace=data)
+        return obs
+        
+class UserField(serializers.WritableField):
+    """
+    Handle references to a User object
+    """
+    def to_native(self, obj):
+        return obj.username
+
+
+    def from_native(self, data):
+        user = User.objects.get(username=data)
+        return user
+        
+class LSRField(serializers.Field):
+    """
+    Handle references to a LastSeenRevision object
+    """
+    def to_native(self, obj):
+        return LastSeenRevisionSerializer(obj).to_native(obj)
+
+class WebHookMappingSerializer(serializers.ModelSerializer):
+
+    lsr = LSRField()
+    obs = BuildServiceField()
+    user = UserField()
+    
     class Meta:
         model = WebHookMapping
-        fields = ('repourl', 'branch', 'project', 'package', 'obs', 'lsr')
-        depth = 2
+        exclude = ('id',)
