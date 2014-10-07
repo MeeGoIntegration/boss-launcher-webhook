ab67c4d3e2186f7f233849de832564fc4c7aa170
David Greaves <david@dgreaves.com>
2014-10-07 10:29:49 +0100

    The /find applies to get and put requests as a detail, not a list. Handles 404 with GET; and both modification and creation with PUT
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- src/webhook_launcher/app/views.py
+++ src/webhook_launcher/app/views.py
@@ -28,7 +28,7 @@ from django.utils import simplejson
 from django.conf import settings
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import list_route, detail_route
+from rest_framework.decorators import list_route, detail_route, permission_classes
 from utils import launch_queue
 from models import WebHookMapping, BuildService, LastSeenRevision
 from serializers import WebHookMappingSerializer, LastSeenRevisionSerializer, BuildServiceSerializer
@@ -114,13 +114,27 @@ class WebHookMappingViewSet(viewsets.ModelViewSet):
     serializer_class = WebHookMappingSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
-    @detail_route(methods=['get'], permission_classes=[permissions.IsAuthenticatedOrReadOnly])
+    @detail_route(methods=['get', 'put'], permission_classes=[permissions.IsAuthenticatedOrReadOnly])
     def find(self, request, obsname, project, package):
-        qs = WebHookMapping.objects.get(obs__namespace=obsname, project=project, package=package)
-        ser = WebHookMappingSerializer(qs)
-        return Response(ser.data)
-        
-
+        if request.method == 'GET':
+            try:
+                qs = WebHookMapping.objects.get(obs__namespace=obsname, project=project, package=package)
+                ser = WebHookMappingSerializer(qs)
+                return Response(ser.data)
+            except WebHookMapping.DoesNotExist:
+                return Response(None)
+        elif request.method == 'PUT':
+            try:
+                obj = WebHookMapping.objects.get(obs__namespace=obsname, project=project, package=package)
+                # The decorator stored our kwargs and doesn's support
+                # chaining very well so append 'pk' to self.kwargs and
+                # then call update()
+                self.kwargs['pk'] = obj.id
+                return self.update(request=request, pk=obj.id)
+            except WebHookMapping.DoesNotExist:
+                return self.create(request=request)
+        else :
+            raise Exception("Invalid method in find()")
     # def pre_save(self, obj):
     #     obj.user = self.request.user
 
