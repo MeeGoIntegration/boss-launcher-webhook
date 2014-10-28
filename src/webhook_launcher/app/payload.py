22010fde215729c84ac3ff60e1a1f668a5a68a8d
David Greaves <david@dgreaves.com>
2014-10-28 13:48:49 +0000

    Handle a payload with no 'ref' in it.
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- src/webhook_launcher/app/payload.py
+++ src/webhook_launcher/app/payload.py
@@ -195,6 +195,9 @@ class GhPush(Payload):
         repourl = self.url
 
         # github performs one POST per ref (tag/branch) touched even if they are pushed together
+        if 'ref' not in payload:
+            print "This payload has no 'ref' in it. Nothing to do."
+            return
         refsplit = payload['ref'].split("/", 2)
         if len(refsplit) > 1:
             reftype, refname = refsplit[1:]
