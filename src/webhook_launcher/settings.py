c40fa93628aec82d244f18f661f435a9fc2b56b1
Islam Amer <islam.amer@jollamobile.com>
2015-03-25 10:45:38 +0200

    fix rest framework filtering for production

--- src/webhook_launcher/settings.py
+++ src/webhook_launcher/settings.py
@@ -263,9 +263,4 @@ if USE_LDAP:
   )
 elif USE_REMOTE_AUTH:
   MIDDLEWARE_CLASSES += ( 'django.contrib.auth.middleware.RemoteUserMiddleware',)
-
-  REST_FRAMEWORK = {
-        'DEFAULT_AUTHENTICATION_CLASSES': (
-            'webhook_launcher.app.auth.RemoteAuthentication',
-        )
-    }
+  REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] =  ('webhook_launcher.app.auth.RemoteAuthentication',)
