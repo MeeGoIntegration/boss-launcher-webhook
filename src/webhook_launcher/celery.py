73376f602a272378112235ee468b3603a2fa20a5
Dmitry Rozhkov <dmitry.rozhkov@jolla.com>
2015-04-17 12:18:34 +0300

    Move celery tasks to webhook_launcher package

---
+++ src/webhook_launcher/celery.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+
+import pika
+import json
+
+from celery import Celery
+from celery.signals import task_success
+
+app = Celery('bureaucrat',
+             broker='amqp://localhost',
+             include=['webhook_launcher.tasks'])
+
+app.conf.update(
+    CELERY_IGNORE_RESULT=True,
+    CELERY_TASK_SERIALIZER='json'
+)
+
+@task_success.connect
+def handle_task_success(sender=None, **kwargs):
+    """Report task results back to workflow engine."""
+
+    parameters = pika.ConnectionParameters(host="localhost")
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.basic_publish(exchange='',
+                          routing_key='bureaucrat_events',
+                          body=json.dumps(kwargs["result"]),
+                          properties=pika.BasicProperties(
+                              delivery_mode=2,
+                              content_type='application/x-bureaucrat-workitem'
+                          ))
+    connection.close()
+
+if __name__ == '__main__':
+    app.start()
---
+++ src/webhook_launcher/tasks.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'webhook_launcher.settings'
+
+from webhook_launcher.app.utils import handle_payload
+from webhook_launcher.app.utils import relay_payload
+from webhook_launcher.celery import app
+
+@app.task
+def handle_webhook(workitem):
+    """Handle POST request to a webhook."""
+
+    payload = workitem["fields"]["payload"]
+    handle_payload(payload)
+
+    return workitem
+
+@app.task
+def relay_webhook(workitem):
+    """Relay webhook POST request to task queue."""
+
+    payload = workitem["fields"]["payload"]
+    relay_payload(payload)
+
+    return workitem
