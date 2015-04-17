29ba85359c4ea35f8d53104dfa19f85da5cbf544
Dmitry Rozhkov <dmitry.rozhkov@jolla.com>
2015-04-17 17:33:42 +0300

    Hide process launching in bureaucrat's Launcher

--- src/webhook_launcher/app/bureaucrat.py
+++ src/webhook_launcher/app/bureaucrat.py
@@ -1,6 +1,6 @@
-import json
-import pika
-import xml.etree.ElementTree as ET
+from __future__ import absolute_import
+
+from bureaucrat.launcher import Launcher
 
 from django.conf import settings
 
@@ -11,25 +11,11 @@ def launch(process_path, fields):
     :param fields: dict of workitem fields
     """
 
-    tree = ET.parse(process_path)
-    proc_elem = tree.getroot()
-    assert proc_elem.tag == 'process'
-
-    fields_elem = ET.Element('fields')
-    fields_elem.text = json.dumps(fields)
-    proc_elem.append(fields_elem)
-    pdef = ET.tostring(proc_elem)
-
-    parameters = pika.ConnectionParameters(host=settings.AMQP_HOST)
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.basic_publish(exchange='',
-                          routing_key=settings.AMQP_ROUTING_KEY,
-                          body=pdef,
-                          properties=pika.BasicProperties(
-                              delivery_mode=2
-                          ))
-    connection.close()
+    launcher = Launcher({
+        "host": settings.AMQP_HOST,
+        "launcher_routing_key": settings.AMQP_ROUTING_KEY
+    })
+    launcher.launch(process_path, fields)
 
 def launch_queue(fields):
     launch(settings.VCSCOMMIT_QUEUE, fields)
