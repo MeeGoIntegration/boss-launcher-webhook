7aba12be51aa712ba651c6ccb3e5105e28af49d1
David Greaves <david@dgreaves.com>
2017-04-01 14:41:53 +0100

    Remove debug print from settings.py where it interferes with django-admin output
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- src/webhook_launcher/settings.py
+++ src/webhook_launcher/settings.py
@@ -80,7 +80,6 @@ if config.has_option('web', 'post_ip_filter'):
             bits = int(bits)
         else:
             bits = 32
-        print "Allow POST from %s / %d" % (ip, bits)
         NETMASKS.append(
             struct.unpack('<L', socket.inet_aton(ip))[0] &
             ((2L << bits-1) - 1)
