35106812c7f4e83f6f17cf0318f449eccddd3e3d
David Greaves <david@dgreaves.com>
2017-05-23 17:25:19 +0100

    Pep8 cleanup and some minor noise to show the participant is active.
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- src/participants/handle_webhook.py
+++ src/participants/handle_webhook.py
@@ -14,7 +14,8 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
 
 """Used to handle an queued webhook event
 
@@ -39,6 +40,7 @@ django.setup()
 
 from webhook_launcher.app.payload import get_payload
 
+
 class ParticipantHandler(object):
     """ Participant class as defined by the SkyNET API """
 
@@ -55,9 +57,11 @@ class ParticipantHandler(object):
         wid.result = False
 
         if wid.fields.payload is None:
-           raise RuntimeError("Missing mandatory field: payload")
+            raise RuntimeError("Missing mandatory field: payload")
 
+        print "Handling a payload"
         payload = get_payload(wid.fields.payload.as_dict())
         payload.handle()
+        print "done"
 
         wid.result = True
