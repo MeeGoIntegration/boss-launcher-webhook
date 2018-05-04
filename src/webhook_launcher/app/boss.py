f7039dc4cda8c9895895827d7cbdc2970b33f0cb
Danis Valiullin <d.valiullin@omprussia.ru>
2018-05-04 17:27:55 +0300

    Fixes #58 Added reading of conf files if exists

--- src/webhook_launcher/app/boss.py
+++ src/webhook_launcher/app/boss.py
@@ -16,8 +16,33 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import json
+import os
+
 from django.conf import settings
 from RuoteAMQP import Launcher
+
+
+def get_process_params(pdef_file_path):
+    """Read process configuration variables if exists
+    :param pdef_file_path: process file path,
+        config file should be with the same name and with '.conf' extension
+    :return Loaded variables or None if couldn't read the conf file
+    """
+    params = {}
+    if os.path.exists(pdef_file_path):
+        file_name, _file_ext = os.path.splitext(pdef_file_path)
+        conf_file_path = file_name + '.conf'
+        try:
+            params = json.load(open(conf_file_path))
+        except IOError:
+            pass
+
+        except ValueError as e:
+            print 'Error while loading json %s:\n%s' % (conf_file_path, str(e))
+
+    return params
+
 
 def launch(process, fields):
     """ BOSS process launcher
@@ -33,8 +58,12 @@ def launch(process, fields):
                         amqp_pass = settings.BOSS_PASS,
                         amqp_vhost = settings.BOSS_VHOST)
 
-    print "launching to (%s,%s)" %(settings.BOSS_HOST, settings.BOSS_VHOST)
-    launcher.launch(pdef, fields)
+    fields_dict = get_process_params(process)
+    fields_dict.update(fields)
+
+    print "launching to (%s,%s)" % (settings.BOSS_HOST, settings.BOSS_VHOST)
+    launcher.launch(pdef, fields_dict)
+
 
 def launch_queue(fields):
     launch(settings.VCSCOMMIT_QUEUE, fields)
